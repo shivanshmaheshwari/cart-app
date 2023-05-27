412ec2a02681a87554d22cddbf98987938b10e55
Shivansh <shivanshmaheshwari99@gmail.com>
2023-05-27 13:10:12 +0530

    add increase quantity fucntion

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,34 +1,66 @@
-import React from 'react';
+import React from "react";
 
-class CartItem extends React.Component{
-    render(){
-        return(
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style = {styles.image}/>
-                </div>
-                <div className="right-block">
-                    <div style = {{fontSize: 25}}>Phone</div>
-                    <div style = {{fontSize: 20}}>RS 999</div>
-                    <div style = {{fontSize: 20}}>Quantity: 1</div>
-
-                    <div className="cart-item-actions">
-                        {/* buttons */}
-
-                    </div>
-                </div>
-            </div>
-        );
+class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "Mobile Phone",
+      quantity: 1,
+      img: "",
     }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+ 
+  increaseQuantity = () => {
+    console.log("this.state",this.state);
+  }
+  render() {
+    const { price, title, quantity } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 22 }}>{title}</div>
+          <div style={{ fontSize: 18 }}>Rs {price}</div>
+          <div style={{ fontSize: 18 }}>Qty:{quantity}</div>
+
+          <div className="cart-item-actions">
+            {/* buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src={require("../src/images/plus.png")}
+              onClick={this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src={require("../src/images/minus.png")}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src={require("../src/images/delete.png")}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
-    }
-}
+  image: {
+    height: 120,
+    width: 120,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
 
 export default CartItem;
+
+// https://static.vecteezy.com/system/resources/previews/011/912/005/original/minus-sign-icon-free-png.png
